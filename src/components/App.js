05e9c8e7d3ac9aec5a1f4141408bc21bbdef1618
santmailo <iamsunil006@gmail.com>
2023-07-19 13:36:46 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 
 import React from "react";
 import './../styles/App.css';
-import Parent from "./Parent";
+import Parent from "parent";
 
-const App = () => {
+const App = () => { 
   return (
     <div>
         {/* Do not remove the main div */}
