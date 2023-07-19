4e335690088adabc61cbffb9a12ba4b8fc9773bb
santmailo <iamsunil006@gmail.com>
2023-07-19 12:13:22 +0530

    submitting code-Wed Jul 19 2023 12:13:17 GMT+0530 (India Standard Time)

---
+++ src/components/child.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Child = (props) => {
+    return (
+        <div className="child">
+            <h2>Child Component</h2>
+            <button onClick={props.buttonClick}>Show Modal</button>
+            {/* Do not remove the main div */
+            props.showModal ? <Modal /> : null
+            }
+
+        </div>
+    )
+}
+
+const Modal = () => {
+    return (
+        <div>
+            <h3>Modal Content</h3>
+            <p>This is the modal content</p>
+        </div>
+    )
+}
+
+export default Child;
---
+++ src/components/parent.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Child from "./Child";
+
+const Parent = () => {
+    const [showModal, setShowModal] = React.useState(false);
+
+    function toggleModal() {
+        setShowModal(!showModal);
+    }
+
+
+    return (
+        <div className="parent">
+            <h1>Parent Compnent</h1>
+            <Child showModal={showModal} buttonClick = {toggleModal}/>
+        </div>
+    )
+}
+
+export default Parent;
